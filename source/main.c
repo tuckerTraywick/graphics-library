45ed4f5b6583f98078cc2066ae7fb293740057c3
Tucker Traywick <tucker.traywick@gmail.com>
2025-09-29 08:53:02 -0400

    Got a window working.

--- source/main.c
+++ source/main.c
@@ -2,6 +2,15 @@
 #include <X11/Xlib.h>
 
 int main(void) {
-	printf("hi\n");
+	Display *main_display = XOpenDisplay(0);
+	Window root_window = XDefaultRootWindow(main_display);
+	Window main_window = XCreateSimpleWindow(main_display, root_window, 0, 0, 800, 600, 0, 0, 0x00000000);
+	XMapWindow(main_display, main_window);
+	XFlush(main_display	);
+
+	while (1) {
+
+	}
+
 	return 0;
 }
