ed0c793b50b79f4920a98a69204c1b29f1e3b166
Tucker Traywick <tucker.traywick@gmail.com>
2025-10-02 08:12:40 -0400

    Got moving line example working.

---
+++ source/graphics.h
@@ -0,0 +1,11 @@
+#ifndef GRAPHICS_H
+#define GRAPHICS_H
+
+struct window {
+	char *name;
+	unsigned int width;
+	unsigned int height;
+	
+};
+
+#endif // GRAPHICS_H
--- source/main.c
+++ source/main.c
@@ -1,16 +1,33 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <X11/Xlib.h>
 
 int main(void) {
-	Display *main_display = XOpenDisplay(0);
+	Display *main_display = XOpenDisplay(NULL);
 	Window root_window = XDefaultRootWindow(main_display);
-	Window main_window = XCreateSimpleWindow(main_display, root_window, 0, 0, 800, 600, 0, 0, 0x00000000);
+	Window main_window = XCreateSimpleWindow(main_display, root_window, 0, 0, 1920, 1080, 0, 0, 0x00000000);
 	XMapWindow(main_display, main_window);
-	XFlush(main_display	);
+	
+	int screen_number = DefaultScreen(main_display);
+	XGCValues values = {
+		.foreground = WhitePixel(main_display, screen_number),
+		.background = WhitePixel(main_display, screen_number),
+		.line_width = 2,
+		.line_style = LineSolid,
+	};
+	unsigned long mask = GCBackground | GCForeground | GCLineWidth | GCLineStyle;
+	GC context = XCreateGC(main_display, main_window, mask, &values);
 
+	int i = 0;
 	while (1) {
-
+		XClearWindow(main_display, main_window);
+		XDrawLine(main_display, main_window, context, 50, 50, i, 400);  // Draw a line from (50,50) to (400,400)
+		XFlush(main_display);
+		usleep(1000);
+		++i;
 	}
 
+	XDestroyWindow(main_display, main_window);
+	XCloseDisplay(main_display);
 	return 0;
 }
