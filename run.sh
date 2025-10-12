cf7eb3d84259260c15862d61b866ec67fb3d2ca8
Tucker Traywick <tucker.traywick@gmail.com>
2025-10-12 01:25:12 -0400

    Got freetype in build.

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 clear
 echo "----BUILDING----"
-gcc -std=gnu99 -Wall -Wextra -Wpedantic -g3 -lX11 source/* -Isource -o build/run && echo && echo "---- RUNNING ----" && ./build/run
+CFLAGS=$(pkg-config --cflags freetype2 x11)
+LIBRARIES=$(pkg-config --libs freetype2 x11)
+gcc -std=gnu99 -Wall -Wextra -Wpedantic -g3 $LIBRARIES -Isource $CFLAGS source/* -o build/run && echo && echo "---- RUNNING ----" && ./build/run
