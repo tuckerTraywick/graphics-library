193ccfc0dadb1cc6ce89035e22a9609afe3f2e22
Tucker Traywick <tucker.traywick@gmail.com>
2025-10-13 13:11:26 -0400

    Re added moving example.

--- source/main.c
+++ source/main.c
@@ -8,17 +8,19 @@ int main(void) {
 		fprintf(stderr, "Couldn't create window.\n");
 		return 1;
 	}
-	if (!window_load_font(window, "dejavu sans mono-16")) {
+	if (!window_load_font(window, "dejavu sans mono-20")) {
 		fprintf(stderr, "Couldn't load font.\n");
 		return 1;
 	}
 
+	int i = 0;
 	while (window_is_open(window)) {
 		window_fill(window, COLOR_BLACK);
 		window_draw_rectangle(window, COLOR_BLUE, 2, 10, 10, 100, 100);
-		window_draw_text(window, "hello world", 20, 20);
+		window_draw_text(window, "hello world", 50, 50);
 		window_flush(window);
 		usleep(1000);
+		i = (i + 1)%800;
 	}
 
 	printf("done\n");
