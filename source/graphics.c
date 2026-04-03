ba080d01bf0efc770d4ff8f881f34f54b1be94eb
Tucker Traywick <tucker.traywick@gmail.com>
2026-04-03 00:13:55 -0400

    Revert "Started refactoring to support multiple backends."
    This reverts commit 48bfe5e518917efd5c65221fc5e21a86ddacef8c.

--- source/graphics.c
+++ source/graphics.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
-#include <stdbool.h>
-#include <stdint.h>
 #include <stddef.h>
+#include <stdbool.h>
 #include <stdlib.h>
 #include <math.h>
 #include <X11/Xlib.h>
 #include <X11/Xft/Xft.h>
 #include "graphics.h"
-#include "graphics_backend.h"
 
 #define abs(x) (((x) < 0) ? (-x) : (x))
 
@@ -17,12 +15,6 @@
 #define min(a, b) (((a) <= (b)) ? (a) : (b))
 
 #define max(a, b) (((a) >= (b)) ? (a) : (b))
-
-struct window {
-	struct backend_window *backend_window;
-	char *name;
-	bool is_open;
-};
 
 struct surface surface_create(struct vector2 size) {
 	struct surface surface = {
@@ -35,19 +27,26 @@ struct surface surface_create(struct vector2 size) {
 	return surface;
 }
 
+struct surface surface_create_with_pixels(pixel *pixels, struct vector2 size) {
+	return (struct surface){
+		.size = size,
+		.pixels = pixels,
+	};
+}
+
 void surface_destroy(struct surface *surface) {
 	free(surface->pixels);
 	*surface = (struct surface){0};
 }
 
-// struct surface surface_get_subsurface(struct surface *surface, struct vector2 offset, struct vector2 size) {
-// 	return (struct surface){
-// 		.size = size,
-// 		.parent_size = surface->size,
-// 		.offset = offset,
-// 		.pixels = surface->pixels,
-// 	};
-// }
+struct surface surface_get_subsurface(struct surface *surface, struct vector2 offset, struct vector2 size) {
+	return (struct surface){
+		.size = size,
+		.parent_size = surface->size,
+		.offset = offset,
+		.pixels = surface->pixels,
+	};
+}
 
 pixel surface_get_pixel(struct surface *surface, struct vector2 position) {
 	if (surface->parent_size.x) {
@@ -56,7 +55,7 @@ pixel surface_get_pixel(struct surface *surface, struct vector2 position) {
 	return surface->pixels[position.y*surface->size.x + position.x];
 }
 
-bool surface_draw_pixel(struct surface *surface, struct vector2 position, pixel color) {
+bool surface_set_pixel(struct surface *surface, struct vector2 position, pixel color) {
 	if (position.x >= surface->size.x || position.y >= surface->size.y) {
 		return false;
 	}
@@ -75,7 +74,7 @@ bool surface_is_valid(struct surface *surface) {
 void surface_fill(struct surface *surface, pixel color) {
 	for (int32_t y = 0; y < surface->size.y; ++y) {
 		for (int32_t x = 0; x < surface->size.x; ++x) {
-			surface_draw_pixel(surface, vec2(x, y), color);
+			surface_set_pixel(surface, vec2(x, y), color);
 		}
 	}
 }
@@ -89,7 +88,7 @@ void surface_draw_line2(struct surface *surface, struct vector2 start, struct ve
 	float x = start.x;
 	float y = start.y;
 	for (uint32_t i = 0; i <= steps; ++i) {
-		if (!surface_draw_pixel(surface, vec2(x, y), color)) {
+		if (!surface_set_pixel(surface, vec2(x, y), color)) {
 			return;
 		}
 		x += x_increment;
@@ -144,7 +143,7 @@ void surface_draw_surface2(struct surface *surface, struct surface *sprite, stru
 			int32_t sprite_y = scale_y*difference_y;
 			int32_t rotated_surface_x = cos_angle*difference_x - sin_angle*difference_y + position.x;
 			int32_t rotated_surface_y = sin_angle*difference_x + cos_angle*difference_y + position.y;
-			surface_draw_pixel(surface, vec2(rotated_surface_x, rotated_surface_y), surface_get_pixel(sprite, vec2(sprite_x, sprite_y)));
+			surface_set_pixel(surface, vec2(rotated_surface_x, rotated_surface_y), surface_get_pixel(sprite, vec2(sprite_x, sprite_y)));
 		}
 	}
 }
@@ -165,37 +164,87 @@ void surface_draw_surface_centered2(struct surface *surface, struct surface *spr
 			float difference_y = surface_y - position.y;
 			int32_t rotated_surface_x = cos_angle*difference_x - sin_angle*difference_y + position.x;
 			int32_t rotated_surface_y = sin_angle*difference_x + cos_angle*difference_y + position.y;
-			surface_draw_pixel(surface, vec2(rotated_surface_x, rotated_surface_y), surface_get_pixel(sprite, vec2(sprite_x, sprite_y)));
+			surface_set_pixel(surface, vec2(rotated_surface_x, rotated_surface_y), surface_get_pixel(sprite, vec2(sprite_x, sprite_y)));
 		}
 	}
 }
 
-struct window *window_create(char *name, struct vector2 position, struct vector2 size) {
-	// Create the window.
-	struct window *window = malloc(sizeof *window);
-	if (!window) {
-		return NULL;
+struct window window_create(char *name, struct vector2 position, struct vector2 size) {
+	struct window window = {
+		.name = name,
+		.size = size,
+		.is_open = true,
+	};
+
+	// Setup the X window.
+	window.x_display = XOpenDisplay(NULL);
+	window.x_window = XCreateSimpleWindow(window.x_display, XDefaultRootWindow(window.x_display), (int)position.x, (int)position.y, size.x, size.y, 0, 0, 0);
+	XMapWindow(window.x_display, window.x_window);
+	int default_screen = DefaultScreen(window.x_display);
+	int default_depth = DefaultDepth(window.x_display, default_screen);
+	Visual *default_visual = DefaultVisual(window.x_display, default_screen);
+	window.resolution = vec2(DisplayWidth(window.x_display, default_screen), DisplayHeight(window.x_display, default_screen));
+
+	// Allocate the framebuffer.
+	window.frame_buffer = calloc(window.resolution.x*window.resolution.y, sizeof *window.frame_buffer);
+	if (!window.frame_buffer) {
+		goto error1;
 	}
 
-	// Create the backend window.
-	*window = (struct window){
-		.name = name,
-		.backend_window = backend_window_create(name, position, size),
-	};
-	if (window->backend_window) {
-		return window;
+	// Setup the XImage.
+	window.x_image = XCreateImage(
+		window.x_display, default_visual, default_depth, ZPixmap, 0, (char*)window.frame_buffer, window.resolution.x, window.resolution.y, 32, 0
+	);
+	if (!window.x_image) {
+		goto error2;
 	}
-	free(window);
-	return NULL;
+
+	// Setup the X graphics context.
+	XGCValues values = {
+		.foreground = WhitePixel(window.x_display, default_screen),
+		.background = BlackPixel(window.x_display, default_screen),
+		.line_width = 1,
+		.line_style = LineSolid,
+	};
+	unsigned long mask = GCBackground | GCForeground | GCLineWidth | GCLineStyle;
+	window.x_context = XCreateGC(window.x_display, window.x_window, mask, &values);
+	window.xft_draw = XftDrawCreate(window.x_display, window.x_window, DefaultVisual(window.x_display, default_screen), DefaultColormap(window.x_display, default_screen));
+
+	// Make it so we can detect the window being closed.
+	window.x_delete_window = XInternAtom(window.x_display, "WM_DELETE_WINDOW", False);
+	if (!XSetWMProtocols(window.x_display, window.x_window, &window.x_delete_window, 1)) {
+		goto error3;
+	}
+
+	return window;
+
+error3:
+	XDestroyImage(window.x_image);
+	XftDrawDestroy(window.xft_draw);
+	XFreeGC(window.x_display, window.x_context);
+error2:
+	free(window.frame_buffer);
+error1:
+	XDestroyWindow(window.x_display, window.x_window);
+	XCloseDisplay(window.x_display);
+	return (struct window){0};
 }
 
 void window_destroy(struct window *window) {
-	backend_window_destroy(window->backend_window);
-	free(window);
+	XDestroyImage(window->x_image);
+	XftDrawDestroy(window->xft_draw);
+	XFreeGC(window->x_display, window->x_context);
+	XDestroyWindow(window->x_display, window->x_window);
+	XCloseDisplay(window->x_display);
+	// free(window->frame_buffer); // Not freeing the frame buffer because `XDestroyImage()` frees it.
 }
 
 struct surface window_get_surface(struct window *window) {
-	return backend_window_get_surface(window->backend_window);
+	return (struct surface){
+		.size = window->size,
+		.parent_size = window->resolution,
+		.pixels = window->frame_buffer,
+	};
 }
 
 bool window_is_open(struct window *window) {
@@ -203,7 +252,17 @@ bool window_is_open(struct window *window) {
 }
 
 void window_update(struct window *window) {
-	if (!backend_window_update(window->backend_window)) {
+	// Display the framebuffer.
+	XPutImage(window->x_display, window->x_window, window->x_context, window->x_image, 0, 0, 0, 0, window->size.x, window->size.y);
+	XFlush(window->x_display);
+
+	// Check for events.
+	if (!XPending(window->x_display)) {
+		return;
+	}
+	XEvent event = {0};
+	XNextEvent(window->x_display, &event); // Blocks.
+	if (event.type == ClientMessage) {
 		window->is_open = false;
 	}
 }
