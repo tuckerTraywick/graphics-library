7aa8f80e7ac0ba6fd9c4c8be6c046908814baf2d
Tucker Traywick <tucker.traywick@gmail.com>
2026-05-14 16:15:46 -0400

    Removed unused code.

--- run.sh
+++ run.sh
@@ -1,9 +1,3 @@
-# clear
-# echo "----BUILDING----"
-# CFLAGS=$(pkg-config --cflags freetype2 x11 xft)
-# LIBRARIES=$(pkg-config --libs freetype2 x11 xft)
-# gcc -std=gnu99 -Wall -Wextra -Wpedantic -g3 $LIBRARIES -Isource $CFLAGS source/* -o build/run && echo && echo "---- RUNNING ----" && ./build/run
-
 set -e
 clear
 make clean
